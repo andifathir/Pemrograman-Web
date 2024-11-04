67ca13b68f21900b74f03ccd8601ca2c600650c4
andifathir <andi.fathirr@gmail.com>
2024-11-04 16:23:01 +0700

    asd

--- Modul3/tugas1/WatchStore/index.php
+++ Modul3/tugas1/WatchStore/index.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/Traits/DiscountTrait.php';
 require_once __DIR__ . '/Models/Watch.php';
 require_once __DIR__ . '/Models/DigitalWatch.php';
-require_once __DIR__ . '/Models/AnalogWatcg.php';
+require_once __DIR__ . '/Models/AnalogWatch.php';
 require_once __DIR__ . '/Controllers/WatchController.php';
 
 use Controllers\WatchController;
